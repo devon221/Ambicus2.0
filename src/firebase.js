2a91520c8ce625fd03ba8f58629aa47d8fdebf2a
Pronoy Chakraborty <68776076+devon221@users.noreply.github.com>
2023-04-17 18:12:05 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,7 @@ import {getAuth} from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDR_t_PFFxvrUeuhzahBYf8AjmrBTLj1x8",
-  authDomain: "ambicus-files.firebaseapp.com",
-  projectId: "ambicus-files",
-  storageBucket: "ambicus-files.appspot.com",
-  messagingSenderId: "82458491534",
-  appId: "1:82458491534:web:a848b300ca4f665369a7df"
+ \\
 };
 
 // Initialize Firebase
